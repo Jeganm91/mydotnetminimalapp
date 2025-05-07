ebcc15f56e87c1439eda0d8d18e8042a099cfd28
Jegannath.M <mjegan@teknoturf.com>
2025-05-07 13:53:34 +0530

    .Net Minimal App

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Content(
+    @"<html>
+      <body style='background-color: lightblue;'>
+        <h1>Welcome to Production Slot!</h1>
+        <p>Thank you</p>
+      </body>
+    </html>", 
+    "text/html"
+));
+
+app.Run();
